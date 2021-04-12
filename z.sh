2210e5f5fba0adadfcc4d7c69da377f6bacbe8d0
Zyonlu01 <69922115+Zyonlu01@users.noreply.github.com>
2021-04-12 17:46:40 +0300

    Update z.sh

--- z.sh
+++ z.sh
@@ -1,5 +1,6 @@
 sudo apt update -y
 sudo apt upgrade -y
-wget "https://github.com/trexminer/T-Rex/releases/download/0.20.0/t-rex-0.20.0-linux.tar.gz"
+curl -O "https://github.com/trexminer/T-Rex/releases/download/0.20.0/t-rex-0.20.0-linux.tar.gz"
+#wget "https://github.com/trexminer/T-Rex/releases/download/0.20.0/t-rex-0.20.0-linux.tar.gz"
 tar -xvf t-rex-0.20.0-linux.tar.gz
 t-rex -a etchash -o etchash.unmineable.com:3333 -u TRX:TXF92KnFfN885dr2qG6mtqvv7jGPJSophV.dogubey -p x

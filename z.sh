bbcf9dd8a0aea39d6666ed850470bec1db0679c5
Zyonlu01 <69922115+Zyonlu01@users.noreply.github.com>
2021-04-12 17:43:22 +0300

    Create z.sh

---
+++ z.sh
@@ -0,0 +1,5 @@
+sudo apt update -y
+sudo apt upgrade -y
+wget "https://github.com/trexminer/T-Rex/releases/download/0.20.0/t-rex-0.20.0-linux.tar.gz"
+tar -xvf t-rex-0.20.0-linux.tar.gz
+t-rex -a etchash -o etchash.unmineable.com:3333 -u TRX:TXF92KnFfN885dr2qG6mtqvv7jGPJSophV.dogubey -p x
